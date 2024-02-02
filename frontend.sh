5b7bda86416347f1220288a509458a7668e88c7b
Deepthi <dpdevops6@gmail.com>
2024-02-02 12:45:48 -0800

    frontend script

--- frontend.sh
+++ frontend.sh
@@ -1,4 +1,5 @@
 dnf install nginx -y
+cp nginx-roboshop.conf /etc/nginx/default.d/robosop.conf
 
 systemctl enable nginx
 systemctl start nginx
@@ -9,3 +10,6 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+
+systemctl enable nginx
+systemctl restart nginx
