e18ff19a13eae4cd15168b9db1d5816adb4bfd6c
Deepthi <dpdevops6@gmail.com>
2024-02-12 17:58:19 -0500

    Roboshop Shell scripting variable

--- catalogue.sh
+++ catalogue.sh
@@ -1,38 +1,40 @@
+log=/tmp/roboshop.log
+
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Create Catalogue Service<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+cp catalogue.service /etc/systemd/system/catalogue.service &>>${log}
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Create Mongodb Repo<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Install nodejs <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-dnf module disable nodejs -y  &>>/tmp/roboshop.log
-dnf module enable nodejs:18 -y  &>>/tmp/roboshop.log
-dnf install nodejs -y  &>>/tmp/roboshop.log
+dnf module disable nodejs -y  &>>${log}
+dnf module enable nodejs:18 -y  &>>${log}
+dnf install nodejs -y  &>>${log}
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Create Application user e<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-useradd roboshop &>>/tmp/roboshop.log
+useradd roboshop &>>${log}
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Create Application Directory<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-mkdir /app &>>/tmp/roboshop.log
+mkdir /app &>>${log}
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Download Application Content<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log}
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Extract Application Content<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 cd /app
-unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+unzip /tmp/catalogue.zip &>>${log}
 cd /app
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Download Nodejs Dependencies<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-npm install &>>/tmp/roboshop.log
+npm install &>>${log}
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Install Mongodb Client<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
+dnf install mongodb-org-shell -y &>>${log}
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Load Catalogue Schema<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-mongo --host mondodb.vdevops562.online </app/schema/catalogue.js &>>/tmp/roboshop.log
+mongo --host mondodb.vdevops562.online </app/schema/catalogue.js &>>${log}
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Start Catalogue Service<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl restart catalogue &>>/tmp/roboshop.log
+systemctl daemon-reload &>>${log}
+systemctl enable catalogue &>>${log}
+systemctl restart catalogue &>>${log}
