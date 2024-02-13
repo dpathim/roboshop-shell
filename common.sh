ee221d63fa1caba1303251e2e8496efe11757afd
Deepthi <dpdevops6@gmail.com>
2024-02-13 15:33:31 -0500

    Roboshop Shell scripting functions

--- common.sh
+++ common.sh
@@ -1,4 +1,28 @@
-nodejs() {
+func_apppreq() {
+  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Create Application User e<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+    useradd roboshop &>>${log}
+
+    echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Cleanup Existing Application Content e<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+      rm-rf / app &>>${log}
+
+    echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Create Application Directory<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+    mkdir /app &>>${log}
+
+    echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Download Application Content<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+
+    echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Extract Application Content<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+    cd /app
+    unzip /tmp/${component}.zip &>>${log}
+    cd /app
+}
+func_systemd() {
+   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Start ${component} Service<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+    systemctl daemon-reload &>>${log}
+    systemctl enable ${component} &>>${log}
+    systemctl restart ${component} &>>${log}
+}
+func_nodejs() {
   log=/tmp/roboshop.log
 
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Create ${component} Service<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
@@ -12,19 +36,7 @@ nodejs() {
   dnf module enable nodejs:18 -y  &>>${log}
   dnf install nodejs -y  &>>${log}
 
-  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Create Application ${component} e<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-  ${component}add roboshop &>>${log}
-
-  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Create Application Directory<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-  mkdir /app &>>${log}
-
-  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Download Application Content<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
-
-  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Extract Application Content<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-  cd /app
-  unzip /tmp/${component}.zip &>>${log}
-  cd /app
+  func_apppreq
 
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Download Nodejs Dependencies<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   npm install &>>${log}
@@ -35,8 +47,27 @@ nodejs() {
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Load ${component} Schema<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   mongo --host mondodb.vdevops562.online </app/schema/${component}.js &>>${log}
 
-  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Start ${component} Service<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-  systemctl daemon-reload &>>${log}
-  systemctl enable ${component} &>>${log}
-  systemctl restart ${component} &>>${log}
+
+}
+
+func_java() {
+  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>create ${component} Service<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  cp shipping.service /etc/systemd/system/shipping.service
+
+  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Install Maven<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  dnf install maven -y
+
+  func_apppreq
+
+  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Build ${component} Service<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  mvn clean package
+  mv target/${component}-1.0.jar ${component}.jar
+
+ echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Install mysql Client<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  dnf install mysql -y
+
+  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Load  Schema<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  mysql -h mysql.vdevops562.online -uroot -pRoboShop@1 < /app/schema/${component}.sql
+
+ func_systemd
 }
