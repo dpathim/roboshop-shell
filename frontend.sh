462936abb023c07e5be8826dab544c4d16cc2f57
Deepthi <dpdevops6@gmail.com>
2024-02-14 12:39:13 -0500

    Roboshop Shell scripting Frontend tags

--- frontend.sh
+++ frontend.sh
@@ -1,15 +1,29 @@
+
+source common.sh
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>install Nginx <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 dnf install nginx -y
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Copy RoboShop Configuration <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 cp nginx-roboshop.conf /etc/nginx/default.d/robosop.conf
+func_exit_status
 
-systemctl enable nginx
-systemctl start nginx
-
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Clean Old Content<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
+func_exit_status
 
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Downloading Application Content <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+func_exit_status
 
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
 
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Extract Application Content<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+unzip /tmp/frontend.zip
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Start Nginx Service<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl enable nginx
 systemctl restart nginx
+func_exit_status
