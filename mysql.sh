b844b2671193429c24fc018d53a5838f6e4153e1
Deepthi <dpdevops6@gmail.com>
2024-02-14 15:55:59 -0500

    Password NFR

--- mysql.sh
+++ mysql.sh
@@ -1,18 +1,25 @@
+source common.sh
 Mysql_root_password=$1
   if [ -z "${Mysql_root_password}" ]; then
     echo INpute Missing Password
     exit
     fi
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Create Mysql Repo<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>{log}
+func_exit_status
 
-cp mysql.repo /etc/yum.repos.d/mysql.repo
-dnf module disable mysql -y
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Install Mysql<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+dnf module disable mysql -y &>>{log}
+dnf install mysql-community-server -y &>>{log}
+func_exit_status
 
-dnf install mysql-community-server -y
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Start Mysql service<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+systemctl enable mysqld &>>{log}
+systemctl start mysqld &>>{log}
+func_exit_status
 
-
-systemctl enable mysqld
-systemctl start mysqld
-
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Mysql Password Inpute<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 mysql_secure_installation --set-root-pass ${Mysql_root_password}
+func_exit_status
 
 #RoboShop@1
