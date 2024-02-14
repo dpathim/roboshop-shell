1383dc40781029da73653004accec72a7ec71f4a
Deepthi <dpdevops6@gmail.com>
2024-02-14 11:52:32 -0500

    Roboshop Shell scripting Exit status

--- common.sh
+++ common.sh
@@ -1,23 +1,36 @@
 log=/tmp/roboshop.log
+func_exit_status() {
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS\e[0m"
+    else
+      echo -e "\e[31m FAILURE\e[0m"
+      fi
+  }
 func_apppreq() {
    echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Create ${component} Service<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
     cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
+    func_exit_status
 
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Create Application User<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   useradd roboshop &>>${log}
+  func_exit_status
 
     echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Cleanup Existing Application Content<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
     rm -rf /app &>>${log}
+    func_exit_status
 
     echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Create Application Directory<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
     mkdir /app &>>${log}
+    func_exit_status
 
     echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Download Application Content<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
     curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+    func_exit_status
 
     echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Extract Application Content<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
     cd /app
     unzip /tmp/${component}.zip &>>${log}
+    func_exit_status
     cd /app
 }
 func_systemd() {
@@ -25,23 +38,28 @@ func_systemd() {
     systemctl daemon-reload &>>${log}
     systemctl enable ${component} &>>${log}
     systemctl restart ${component} &>>${log}
+    func_exit_status
 }
 
 func_schema_setup() {
   if [ "${schema_type}" == "mongodb" ]; then
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Install Mongodb Client<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
     dnf install mongodb-org-shell -y &>>${log}
+    func_exit_status
 
     echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Load ${component} Schema<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
     mongo --host mondodb.vdevops562.online </app/schema/${component}.js &>>${log}
+    func_exit_status
     fi
 
     if [ "${schema_type}" == "mysql" ]; then
       echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Install mysql Client<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
         dnf install mysql -y &>>${log}
+        func_exit_status
 
         echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Load Schema<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
         mysql -h mysql.vdevops562.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
+        func_exit_status
         fi
 }
 func_nodejs() {
@@ -49,15 +67,18 @@ func_nodejs() {
  func_apppreq
  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Create Mongodb Repo<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
    cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
+   func_exit_status
  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Install nodejs <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   dnf module disable nodejs -y  &>>${log}
   dnf module enable nodejs:18 -y  &>>${log}
   dnf install nodejs -y  &>>${log}
+  func_exit_status
 
   func_apppreq
 
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Download Nodejs Dependencies<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   npm install &>>${log}
+  func_exit_status
 
 func_schema_setup
 func_systemd
@@ -70,12 +91,14 @@ func_java() {
 
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Install Maven<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   dnf install maven -y &>>${log}
+  func_exit_status
 
   func_apppreq
 
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Build ${component} Service<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   mvn clean package &>>${log}
   mv target/${component}-1.0.jar ${component}.jar &>>${log}
+  func_exit_status
 
  func_schema_setup
 
@@ -86,19 +109,23 @@ func_java() {
 func_python() {
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Build ${component} service<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   dnf install python36 gcc python3-devel -y &>>${log}
+  func_exit_status
   func_apppreq
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Build ${component} service<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   pip3.6 install -r requirements.txt &>>${log}
+  func_exit_status
 
   func_systemd
 }
 func_golang() {
    echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Install golang <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   dnf install golang -y &>>${log}
+  func_exit_status
   func_apppreq
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Build ${component}service<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   go mod init dispatch &>>${log}
   go get &>>${log}
   go build &>>${log}
+  func_exit_status
    func_systemd
 }
