e20d874cda0da5576b46d34f8e31f7e87b87b658
Deepthi <dpdevops6@gmail.com>
2024-02-14 16:45:10 -0500

    Password NFR

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -8,17 +8,26 @@ exit 1
 fi
 
 
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Install rabbitmq server<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>${log}
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>${log}
 
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+dnf install rabbitmq-server -y &>>${log}
+func_exit_status
 
-dnf install rabbitmq-server -y
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Install rabbitmq server<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+systemctl enable rabbitmq-server &>>${log}
+systemctl start rabbitmq-server &>>${log}
+func_exit_status
 
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>create rabbitmq user and password <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+rabbitmqctl add_user roboshop ${rabbitmq_app_password} &>>${log}
+func_exit_status
 
-rabbitmqctl add_user roboshop ${rabbitmq_app_password}
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Set  rabbitmq Permission<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${log}
+func_exit_status
+
 
 
 #roboshop123
