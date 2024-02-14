d001177a71f9383539c2407a38e121915310b281
Deepthi <dpdevops6@gmail.com>
2024-02-14 16:22:29 -0500

    Password NFR

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,3 +1,11 @@
+rabbitmq_app_password=$1
+if [ -z "${rabbitmq_app_password}" ]; then
+echo Inpute RabbitMQ AppUser password Missing
+exit 1
+fi
+
+
+
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
 
@@ -6,5 +14,8 @@ dnf install rabbitmq-server -y
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
 
-rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl add_user roboshop ${rabbitmq_app_password}
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
+
+#roboshop123
