fd1db21f4e204b1f0b16bae3a6f7c4a16e145492
Deepthi <dpdevops6@gmail.com>
2024-02-14 12:41:34 -0500

    Roboshop Shell scripting Frontend tags

--- frontend.sh
+++ frontend.sh
@@ -2,28 +2,28 @@
 source common.sh
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>install Nginx <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-dnf install nginx -y
+dnf install nginx -y &>>${log}
 func_exit_status
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Copy RoboShop Configuration <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-cp nginx-roboshop.conf /etc/nginx/default.d/robosop.conf
+cp nginx-roboshop.conf /etc/nginx/default.d/robosop.conf &>>${log}
 func_exit_status
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Clean Old Content<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>${log}
 func_exit_status
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Downloading Application Content <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
 func_exit_status
 
 cd /usr/share/nginx/html
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Extract Application Content<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>${log}
 func_exit_status
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>Start Nginx Service<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &>>${log}
+systemctl restart nginx &>>${log}
 func_exit_status
