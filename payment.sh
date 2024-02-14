46b8480ee769e690cc725d6699e8d246cea6a921
Deepthi <dpdevops6@gmail.com>
2024-02-14 16:15:45 -0500

    Password NFR

--- payment.sh
+++ payment.sh
@@ -1,5 +1,12 @@
 component=payment
 source common.sh
 
+rabbitmq_app_password=$1
+if [ -z "${rabbitmq_app_password}" ]; then
+echo Inpute RabbitMQ AppUser password Missing
+exit 1
+fi
 func_python
 
+
+#Environment=AMQP_PASS=roboshop123
